dc4d41ef5d4050ff3bd00c0db6d588fb8e272b63
RJ Russell <RJ-Russell@users.noreply.github.com>
2018-07-28 14:04:49 -0600

    phone-number: first test set 'number-tests' passing.

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,11 +1,27 @@
 (ns phone-number)
 
-(defn number [num-string] ;; <- arglist goes here
-      ;; your code goes here
-      )
+;; Length less than 10, return all zeros
+;; Lenght 11, but first character is not a 1, return all zeros
+;; Length 11, first character is a one, return the string minus the first character
+;; Otherwise return the string
 
-(defn area-code [num-string] ;; <- arglist goes here
-  ;; your code goes here
+(defn invalid-number?
+  [s]
+  (let [num-size (count s)]
+    (or
+     (< num-size 10)
+     (> num-size 11)
+     (and
+      (= num-size 11) (not= (first s) "1")))))
+
+(defn number [num-string]
+  (let [n-coll (re-seq #"\d" num-string)]
+    (if (invalid-number? n-coll)
+      "0000000000"
+      (apply str
+             (if (= 11 (count n-coll)) (rest n-coll) n-coll)))))
+
+(defn area-code [num-string] 
   )
 
 (defn pretty-print [num-string] ;; <- arglist goes here
--- clojure/phone-number/test/phone_number_test.clj
+++ clojure/phone-number/test/phone_number_test.clj
@@ -2,38 +2,20 @@
   (:require [clojure.test :refer [deftest is]]
             phone-number))
 
-(deftest cleans-number
-  (is (= "1234567890" (phone-number/number "(123) 456-7890"))))
-
-(deftest cleans-number-with-dots
-  (is (= "5558675309" (phone-number/number "555.867.5309"))))
-
-(deftest valid-when-11-digits-and-first-is-1
-  (is (= "9876543210" (phone-number/number "19876543210"))))
-
-(deftest invalid-when-11-digits
-  (is (= "0000000000" (phone-number/number "21234567890"))))
-
-(deftest invalid-when-9-digits
+(deftest number-tests
+  (is (= "1234567890" (phone-number/number "(123) 456-7890")))
+  (is (= "5558675309" (phone-number/number "555.867.5309")))
+  (is (= "9876543210" (phone-number/number "19876543210")))
+  (is (= "0000000000" (phone-number/number "21234567890")))
   (is (= "0000000000" (phone-number/number "123456789"))))
 
-(deftest area-code
-  (is (= "123" (phone-number/area-code "1234567890"))))
-
-(deftest area-code-with-dots
-  (is (= "555" (phone-number/area-code "555.867.5309"))))
-
-(deftest area-code-with-parentheses
-  (is (= "987" (phone-number/area-code "(987) 654-3210"))))
-
-(deftest area-code-with-full-us-phone-number
+(deftest area-code-tests
+  (is (= "123" (phone-number/area-code "1234567890")))
+  (is (= "555" (phone-number/area-code "555.867.5309")))
+  (is (= "987" (phone-number/area-code "(987) 654-3210")))
   (is (= "123" (phone-number/area-code "11234567890"))))
 
-(deftest pretty-print
-  (is (= "(123) 456-7890" (phone-number/pretty-print "1234567890"))))
-
-(deftest pretty-print-with-dots
-  (is (= "(555) 867-5309" (phone-number/pretty-print "555.867.5309"))))
-
-(deftest pretty-print-with-full-us-phone-number
+(deftest pretty-print-tests
+  (is (= "(123) 456-7890" (phone-number/pretty-print "1234567890")))
+  (is (= "(555) 867-5309" (phone-number/pretty-print "555.867.5309")))
   (is (= "(987) 654-3210" (phone-number/pretty-print "19876543210"))))

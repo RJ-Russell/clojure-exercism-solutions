d0dd272a5b4dd736c11f9ccc68372fb16c59620d
RJ Russell <RJ-Russell@users.noreply.github.com>
2018-08-01 17:16:33 -0600

    rna-transcription: refactor code to single function

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,9 +1,7 @@
 (ns rna-transcription)
 
-(defn get-complement
-  [n]
-  (get {\G "C" \C "G" \T "A" \A "U"} n))
+(def rna-map {\G "C" \C "G" \T "A" \A "U"})
 
 (defn to-rna [dna]
-  {:pre [(not (re-seq #"[^AGCT]" dna))]}
-  (clojure.string/join (map get-complement dna)))
+  {:pre [(every? #(contains? rna-map %) dna)]}
+  (clojure.string/join (map #(get rna-map %) dna)))

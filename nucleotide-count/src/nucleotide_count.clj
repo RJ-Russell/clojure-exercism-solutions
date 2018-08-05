0e853343d8e916d23bb24d6a80e9201fdc1dfc62
RJ Russell <RJ-Russell@users.noreply.github.com>
2018-08-05 09:30:04 -0600

    nucleotide-count: remove redundant code

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,10 @@
 (ns nucleotide-count)
 
+(defn nucleotide-counts
+  [strand]
+  (merge {\A 0 \T 0 \C 0 \G 0} (frequencies strand)))
+
 (defn count [nucleotide strand]
   {:post [((complement nil?) %)]}
-  (if (clojure.string/blank? strand) 0 (get (frequencies strand) nucleotide)))
+  (get (nucleotide-counts strand) nucleotide))
 
-
-(defn nucleotide-counts
-[strand]
-(merge {\A 0 \T 0 \C 0 \G 0} (frequencies strand)))
